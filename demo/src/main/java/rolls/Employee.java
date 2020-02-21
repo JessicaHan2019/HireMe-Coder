2eab76d2db8c3c16fbfc65bd78997f22bf2954b0
Jialun Han <hanjialunjessica@gmail.com>
2020-02-21 15:21:06 +1030

    Initialize processing

---
+++ demo/src/main/java/rolls/Employee.java
@@ -0,0 +1,26 @@
+package rolls;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+
+public class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String branch;
+
+    Employee(){}
+
+    Employee(String name, String branch){
+        this.name = name;
+        this.branch = branch;
+    }
+
+
+}
---
+++ demo/src/main/java/rolls/RollsApplication.java
@@ -0,0 +1,12 @@
+package rolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RollsApplication {
+    public static void main(String... args){
+        SpringApplication.run(RollsApplication.class, args);
+    }
+}
