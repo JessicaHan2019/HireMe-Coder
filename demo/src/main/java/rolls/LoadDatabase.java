4461d5921dc55632949c2cc5621d93897615079f
Jialun Han <hanjialunjessica@gmail.com>
2020-02-23 13:40:02 +1030

    Complete RPC

--- demo/src/main/java/rolls/LoadDatabase.java
+++ demo/src/main/java/rolls/LoadDatabase.java
@@ -14,7 +14,7 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase(EmployeeRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Jessica", "burglar")));
             log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
         };
     }
